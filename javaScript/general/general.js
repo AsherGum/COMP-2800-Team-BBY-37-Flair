a2d635c0d59abf11d75ac8f8058a1c8e079acc9d
Ben Chin <benchin86@gmail.com>
2020-05-14 21:03:28 -0700

    categories now converts query value into the full category name and displays it correctly

--- javaScript/general/general.js
+++ javaScript/general/general.js
@@ -20,3 +20,44 @@ function parseSearchURL() {
     return searchQueries;
 }
 
+
+/**
+ * Returns the original long version of the 
+ * category using the category value in the DB.
+ * 
+ * ie: 'talent' becomes 'Skills/Talents'
+ * 
+ * @param {string} categoryValue 
+ */
+function convertCategoryValue(categoryValue) {
+    switch (categoryValue) {
+        case "physical":
+            return "Physical";
+            break;
+        case "talent":
+            return "Skills/Talents";
+            break;
+        case "comedy":
+            return "Comedy";
+            break;
+        case "games":
+            return "Video Games";
+            break;
+        case "pets":
+            return "Pets";
+            break;
+        case "arts":
+            return "Arts and Crafts";
+            break;
+        case "edu":
+            return "Education";
+            break;
+        case "other":
+            return "Other";
+            break;
+        default:
+            return categoryValue;
+    }
+
+
+}
